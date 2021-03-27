49e335e520536bbb32d58275cce908aeb73de88f
lemon234071 <79388548@qq.com>
2021-03-27 22:20:39 +0800

    Update split_multi_repost

--- rules/str_level.py
+++ rules/str_level.py
@@ -138,8 +138,12 @@ def bert_clean(text):
 
 def split_multi_repost(utter):
     # 一起来吗？@Cindy //@Bob: 算我一个//@Amy: 今晚开派对吗？
-    if utter.count("//@") > 0:
-        return [x.strip() for x in utter.split("//") if len(x.strip()) > 0]
+    if utter.find("//@") > -1:
+        utters = [x.strip() for x in utter.split("//@")]
+        for i in range(1, len(utters)):
+            if utters[i]:
+                utters[i] = "@" + utters[i]
+        return utters
     return [utter]
 
 
