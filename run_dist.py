716a06cbdc7b5b5cba06217218a217a33204b283
lemon234071 <79388548@qq.com>
2021-03-29 19:44:21 +0800

    Fix logging bug

--- run_dist.py
+++ run_dist.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 from single_filter import main_filter, add_filter_args
 from data_utils import *
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')  # - %(name)s
 logger = logging.getLogger(__file__)
 
 
